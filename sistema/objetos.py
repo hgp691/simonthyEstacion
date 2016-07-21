7a56b4171c4d4617266b1324ec1a908c37a12fef
hgp691 <hgp691@gmail.com>
2016-07-20 23:16:25 -0500

    Create objetos.py

---
+++ sistema/objetos.py
@@ -0,0 +1,6 @@
+class TyH:
+        def __init__(self,fecha,hora,temperatura,humedad):
+                self.fecha=fecha
+                self.hora=hora
+                self.temperatura=temperatura
+                self.humedad=humedad
