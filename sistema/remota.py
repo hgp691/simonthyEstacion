06074ac3e894ac440316df54018176bba62a5551
hgp691 <hgp691@gmail.com>
2016-07-21 15:23:24 -0500

    Update remota.py

--- sistema/remota.py
+++ sistema/remota.py
@@ -7,27 +7,27 @@ import log
 
 
 def obtenerRemota(obj):
-	puerto=obj["PUERTO"]
-	velocidad=obj["VELOCIDAD"]
-	try:
-		ser = serial.Serial(puerto, velocidad, timeout=2)
-		ser.close()
-		ser.open()
-		ser.write(obj["DIRECCION"])
-		time.sleep(3)
-		ser.flush()
-		print "Imprimiendo dato"
-		read_val=ser.read(size=64)
-		print read_val
-		ser.flush()
-		print "* "+str(len(read_val))
-		ser.close()
-	except serial.SerialException:
-		print "Exeption"	
-		res0="Excepcion 0 correo.correo ->",sys.exc_info()[0]
-      		print res0
-        	res="Excepcion 1 correo.correo -> ",sys.exc_info()[1]
-        	print res
-        	log.escribirLog(res0)
-        	log.escribirLog(res)
-
+        puerto=obj["PUERTO"]
+        velocidad=obj["VELOCIDAD"]
+        try:
+                ser = serial.Serial(puerto, velocidad, timeout=2)
+                ser.close()
+                ser.open()
+                ser.write(obj["DIRECCION"])
+                time.sleep(3)
+                ser.flush()
+                print "Imprimiendo dato"
+                read_val=ser.read(size=64)
+                print read_val
+                ser.flush()
+                print "* "+str(len(read_val))
+                ser.close()
+        except serial.SerialException:
+                print "Exeption"
+                aaa="Excepcion remota.obtenerRemota "+obj["NOMBRE"]+" -> "
+                res0=aaa,sys.exc_info()[0]
+                print res0
+                res=aaa,sys.exc_info()[1]
+                print res
+                log.escribirLog(res0)
+                log.escribirLog(res)
