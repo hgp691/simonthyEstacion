e747918ea41cd03c98d9ebbcb3114874ea13e75c
hgp691 <hgp691@gmail.com>
2016-07-20 23:03:58 -0500

    Update json1.py

--- sistema/json1.py
+++ sistema/json1.py
@@ -39,3 +39,25 @@ def obtenFechaActual():
 	fecha=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
 	print fecha
 	return fecha
+def obtenerSetTemp():
+        ruta = obtenerRutaConfiguracion() + "sets.json"
+        with open(ruta) as data_file:
+                data=json.load(data_file)
+                return data["TEMPERATURA"]
+def obtenerSetHum():
+        ruta = obtenerRutaConfiguracion() + "sets.json"
+        with open(ruta) as data_file:
+                data=json.load(data_file)
+                return data["HUMEDAD"]
+def obtenerArreglo(archivo):
+        ruta='/mnt/usb/simonthy/datos/'+archivo
+        arreglo=[]
+        reader=csv.reader(open(ruta,'rb'))
+        for index,row in enumerate(reader):
+                obj["fecha"]=row[0]
+                obj["hora"]=row[1]
+                obj["temperatura"]=row[2]
+                obj["humedad"]=row[3]
+                arreglo.append(obj)
+        return arreglo
+obtenerArreglo("local.csv")
